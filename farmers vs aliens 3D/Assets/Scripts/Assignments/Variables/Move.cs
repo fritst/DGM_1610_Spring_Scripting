709abe5273c7813c399be16060acb8576fe264b0
fritst <wiltessers@gmail.com>
2020-01-22 17:14:25 -0700

    Added players, obstacles, ground, added collision.

---
+++ farmers vs aliens 3D/Assets/Scripts/Assignments/Variables/Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0.4f, 0, 0);
+    }
+
+    //Detect collision with another object
+    void OnCollisionEnter(Collision other){
+
+
+    }
+
+    void OnTriggerEnter(Collider other){
+     
+        
+    }
+
+}
