07ac40182fe3312ab80b85df8db520c884a80db3
fritst <wiltessers@gmail.com>
2020-02-12 17:28:37 -0700

    health pickups created and inheritance as well as enemy manager and move

---
+++ farmers vs aliens 3D/Assets/Scripts/Assignments/Variables/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemyPreFabs;
+    public int enemyIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //int enemyIndex = Random.Range(0, enemyPreFabs.Length);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+           
+            Instantiate(enemyPreFabs[enemyIndex], new Vector3(0, 0, 0), enemyPreFabs[enemyIndex].transform.rotation);
+
+        }
+
+
+    }
+}
