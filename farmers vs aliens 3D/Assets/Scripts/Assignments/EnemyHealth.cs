ccd57581a6ce5c19247c7a78ae1a244ef6e1b0d3
fritst <wiltessers@gmail.com>
2020-02-26 17:33:30 -0700

    Added bullet and camera follow

---
+++ farmers vs aliens 3D/Assets/Scripts/Assignments/EnemyHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth = 3;
+    public Transform spawnPoint;
+    public int points = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth -= maxHealth;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void TakeDamage(int amount)
+    {
+        if (currentHealth <= 0)
+        {
+            //keep score at zero
+            currentHealth = 0;
+            print("Enemy is Dead!");
+            // add points to your score for killing wolf
+            //scoremanager.AddPoints(points);
+            //Move wolf to spawn point for restart
+            transform.position = spawnPoint.position;
+            transform.rotation = spawnPoint.rotation;
+            //reset Wolf Health
+            currentHealth = maxHealth;
+
+
+        }
+    }
+}
+
