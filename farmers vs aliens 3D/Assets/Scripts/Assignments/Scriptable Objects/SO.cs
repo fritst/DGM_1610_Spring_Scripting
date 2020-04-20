4a0acfc2b4514ec7e01a2a594ed078aa47d32bef
Frits <wiltessers@gmail.com>
2020-04-20 00:07:25 -0600

    lots of scriptable objects

---
+++ farmers vs aliens 3D/Assets/Scripts/Assignments/Scriptable Objects/SO.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This following line of code adds our Scriptable Object as an option in the asset menu.
+[CreateAssetMenu(fileName ="New SO", menuName ="New Scriptable Object")]
+
+public class SO : ScriptableObject
+{
+    public string soName;
+    public string description;
+
+    public GameObject model;
+    public Color color;
+
+    //Scriptable Objects do not use monobehavior callsbacks such as Start() and Update().
+
+    //Scritable objects can also contain functions and conditionals.
+
+    /*private void PrintOut(string name, string description){
+      Debug.Log(name +"  "+ description);
+      if(name == "Check Norris"){
+        Debug.Log("Chuck Norris allows this program to run!");
+      }
+    }
+    */
+}
